5812a67d0f55000d6a6e91261f7efa3f3c6444fa
bagguo <bagguo23@gmail.com>
2021-09-28 21:51:47 +0800

    MyUniswp2的路由合约地址

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,9 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
 // export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
-export const ROUTER_ADDRESS = '0xB9558Dae7Cf441c51184C06131457Bf584e986B4'
+// export const ROUTER_ADDRESS = '0xB9558Dae7Cf441c51184C06131457Bf584e986B4' // mySwap 的路由合约地址
+export const ROUTER_ADDRESS = '0xC2606927D270554AAb19F79De55348e629a82a8A'
+
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
